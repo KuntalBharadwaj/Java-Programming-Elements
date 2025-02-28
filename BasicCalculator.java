54db471882b59f511432ff52872a55c6dc183e8c
KuntalBharadwaj <kuntalbharadwaj18@gmail.com>
2025-02-28 17:00:41 +0530

    [Kuntal] add: level-1 question no. 11

--- BasicCalculator.java
+++ BasicCalculator.java
@@ -4,22 +4,17 @@ public class BasicCalculator {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // Taking input
         System.out.print("Enter first number: ");
         double number1 = input.nextDouble();
         System.out.print("Enter second number: ");
         double number2 = input.nextDouble();
 
-        // Performing calculations
         double addition = number1 + number2;
         double subtraction = number1 - number2;
         double multiplication = number1 * number2;
         double division = number1 / number2;
 
-        // Displaying results
-        System.out.println("The addition, subtraction, multiplication, and division of numbers " + 
-                           number1 + " and " + number2 + " are " + addition + ", " + subtraction + 
-                           ", " + multiplication + ", and " + division);
-        input.close();
+        System.out.println("The addition, subtraction, multiplication, and division of numbers " + number1 + " and " + number2 + " are " + addition + ", " + subtraction + 
+        ", " + multiplication + ", and " + division);
     }
 }
