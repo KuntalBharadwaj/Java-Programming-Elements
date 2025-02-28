20d0959d3b961c33d64152365138675ec775e0de
KuntalBharadwaj <kuntalbharadwaj18@gmail.com>
2025-02-28 11:08:45 +0530

    [Kuntal] add: converting killometer to miles

---
+++ ConvertKilometerToMiles.java
@@ -0,0 +1,10 @@
+class ConvertKilometerToMiles {
+	public static void main(String[] args) {
+		float killometer = 10.8f;
+		
+		System.out.print("The distance ");
+		System.out.print(killometer);
+		System.out.print(" in miles is ");
+		System.out.print(10.8/1.6);
+	}
+}
