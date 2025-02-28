dc742964a8ae2fc14d11118754ecb1bf7615b64f
KuntalBharadwaj <kuntalbharadwaj18@gmail.com>
2025-02-28 11:06:15 +0530

    [Kuntal] add: finding age of harry

---
+++ FindAgeOfHarry.java
@@ -0,0 +1,7 @@
+class FindAgeOfHarry {
+	public static void main(String[] args) {
+		int harryBirthyear = 2000;
+		System.out.print("Harry's age in 2024 is : ");
+		System.out.print(2024 - harryBirthyear);
+	}
+}
