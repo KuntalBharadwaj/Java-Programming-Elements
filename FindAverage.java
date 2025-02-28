19b95bac4b7e12871a0f686a807f0c46423bef2e
KuntalBharadwaj <kuntalbharadwaj18@gmail.com>
2025-02-28 11:07:43 +0530

    [Kuntal] add: finding average of sam's mark

---
+++ FindAverage.java
@@ -0,0 +1,10 @@
+class FindAverage {
+	public static void main(String[] args) {
+		int Math = 94;
+		int physics = 95;
+		int chemistry = 96;
+		
+		System.out.print(" Sam average mark in PCM is : ");
+		System.out.print((Math+physics+chemistry)/3);
+	}
+}
